88c489fb7de340673b2e9eb301b7881bf4c8a5b6
EnzoFamiliar-Marais <enzo.familiar-marais@etu.univ-orleans.fr>
2024-12-08 00:32:11 +0100

    Ajout des premiers fichiers du site

---
+++ home.js
@@ -0,0 +1,7 @@
+let bt1 = document.getElementById('1');
+
+bt1.onclick = () => { 
+    bt1.style.fontFamily = 'Arial Black';
+    bt1.style.fontSize = '200px';
+    bt1.style.backgroundColor = 'white';
+}
